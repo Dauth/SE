a1564b48a02afb57addbd6f89bbd50b5071490d2
gary <garywjd91@gmail.com>
2015-08-23 00:26:53 +0800

    completed functions in input class

--- src/Input.java
+++ src/Input.java
@@ -1,4 +1,25 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 
 public class Input {
-
+	Storage storage;
+	private static Input mInput = null;
+	
+	public Input(){
+		storage = Storage.getInstance();
+	}
+	
+	public Input getInstance(){
+		if (mInput == null){
+			mInput = new Input();
+		}
+		return mInput;
+	}
+	
+	public void setup(String inputLoc, String ignoreLoc) throws FileNotFoundException, IOException{
+		storage.loadInputFile(inputLoc, "input");
+		storage.loadInputFile(ignoreLoc, "ignore");
+	}
+	
 }
