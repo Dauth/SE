2ccef8ceeaba6563d411417834c4dce9fb3f5fcf
gary <garywjd91@gmail.com>
2015-08-22 23:50:29 +0800

    finished Output class

--- src/Output.java
+++ src/Output.java
@@ -1,4 +1,27 @@
+import java.util.ArrayList;
+
 
 public class Output {
-
+	private AlphabeticSort alphabeticSort;
+	private ArrayList<String> completeList;
+	private static Output mOutput = null;
+	
+	public Output(){
+		this.alphabeticSort = AlphabeticSort.getInstance();
+		this.completeList = alphabeticSort.getSortedList();
+	}
+	
+	public Output getInstance(){
+		if (mOutput == null){
+			mOutput = new Output();
+		}
+		return mOutput;
+	}
+	
+	public void print(){
+		System.out.println("-----Completed-----");
+		for (int i = 0; i < completeList.size(); i++) {
+			System.out.println(completeList.get(i));
+		}
+	}
 }
