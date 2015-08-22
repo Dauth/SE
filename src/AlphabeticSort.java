c82f12a40daa689818c570b201a51fdc194d19cd
gary <garywjd91@gmail.com>
2015-08-22 23:18:05 +0800

    added singleton circularshift

--- src/AlphabeticSort.java
+++ src/AlphabeticSort.java
@@ -1,4 +1,4 @@
 
-public class AlphabeticShift {
+public class AlphabeticSort {
 
 }
--- src/CircularShift.java
+++ src/CircularShift.java
@@ -4,17 +4,25 @@ import java.util.Vector;
 
 public class CircularShift {
 	Storage storage;
+	private static final CircularShift mcircularShift = new CircularShift();
 	private ArrayList<String> shiftedLlineList;
 	
+	public CircularShift(){
+		setup();
+	}
 	
 	public void setup(){
-		storage = Storage.getInstance();
+		this.storage = Storage.getInstance();
 		int inputListLength = storage.getLengthInputData();
-		shiftedLlineList = new ArrayList<String>();
+		this.shiftedLlineList = new ArrayList<String>();
 		for (int i = 0; i < inputListLength; i++) {
 			String line = storage.getLineAtPos(i);
 			shiftedLlineList.addAll(shiftLineInList(line));
 		}
+	}
+	
+	public static CircularShift getInstance(){ 
+		return mcircularShift;
 	}
 	
 	private ArrayList<String> shiftLineInList(String extractedLine){
